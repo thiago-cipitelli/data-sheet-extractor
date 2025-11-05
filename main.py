2238efb7a418516fa5f4e423ef690207c366babf
Thiago Cipitelli <cipitelliprata@gmail.com>
2025-11-05 16:40:20 -0300

    feat: procura diferentes nomes para codigo ean

--- main.py
+++ main.py
@@ -3,14 +3,37 @@ import pandas as pd
 from produto import Produto
 
 
-def get_header_index(df: pd.DataFrame) -> int:
-    mask = df.apply(lambda col: col.astype(str).str.contains("EAN", case=False, na=False))
+variacoes_codigo_barras = [
+    "Codigo_Barra",
+    "Código_Barra",
+    "cod_barra",
+    "cód barra",
+    "cod barra",
+    "codigo de barra",
+    "código de barra",
+    "cod. barra",
+    "codbarra",
+    "codigo_barra",
+    "codigo_bar",
+    "codbarra",
+    "codbar",
+    "EAN"
+]
+
+def get_column(df: pd.DataFrame):
+    colunas_lower = [col.lower() for col in variacoes_codigo_barras]
+
+    for col in df.columns:
+        for possivel_col in colunas_lower:
+            if possivel_col in str(col).lower():
+                return col
+
+
+def get_header_index(df: pd.DataFrame):
+    pattern = '|'.join([v.replace('.', r'\.') for v in variacoes_codigo_barras])
+    mask = df.apply(lambda col: col.astype(str).str.contains(pattern, case=False, na=False, regex=True))
     header_index = mask.any(axis=1)[::-1].idxmax()
     return header_index
-
-def get_column(name: str, columns: list):
-    column = [item for item in columns if isinstance(item, str) and name in item.upper()]
-    return column[0]
 
 def produto_unico(ean: str, lista_prods: list[Produto]) -> bool:
     for prod in lista_prods:
@@ -24,9 +47,10 @@ def extract_products(file, sheet, produtos):
     df = pd.read_excel(file, sheet_name=sheet, header=None)
     header_index = get_header_index(df)
     df = pd.read_excel(file, sheet_name=sheet, header=header_index)
+    ean_column = get_column(df)
     print(sheet)
     if not df.empty:
-        ean_column = get_column('EAN', df.columns)
+        print(ean_column)
         for index, row in df.iterrows():
             if not pd.isna(row[ean_column]) and produto_unico(row[ean_column], produtos):
                 produtos.append(Produto(row[ean_column]))
