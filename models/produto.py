3c406ddf02498dc913637d3dc95defb0123bc438
Thiago Cipitelli <cipitelliprata@gmail.com>
2025-11-13 11:41:43 -0300

    feat: retorna total de vendas do produto

--- models/produto.py
+++ models/produto.py
@@ -8,8 +8,15 @@ class Produto:
     def adicionar_venda(self, venda):
         self.vendas.append(venda)
 
+    def vendas_total(self):
+        total = 0
+        for venda in self.vendas:
+            total += venda.quantidade
+
+        return total
+
     def __str__(self):
-        return f"Produto(EAN={self.ean}, Desc={self.descricao}, Estoque={self.estoque})"
+        return f"Produto(EAN={self.ean}, Desc={self.descricao}, Estoque={self.estoque}, Vendas={self.vendas_total()})"
 
     def __repr__(self):
         return self.__str__()
