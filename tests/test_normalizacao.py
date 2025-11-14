429172ad5607358d433e948034bbf80baefcb06a
Thiago Cipitelli <cipitelliprata@gmail.com>
2025-11-14 19:19:39 -0300

    test: cria testes para normalizacao de data

--- tests/test_normalizacao.py
+++ tests/test_normalizacao.py
@@ -3,16 +3,20 @@ from utils.normalizacao import *
 
 
 def test_remove_numeros_e_simbolos():
-    texto = "123teste ./23@teste"
-
-    result = remove_numeros_e_simbolos(texto)
-    assert result == "teste teste"
+    assert remove_numeros_e_simbolos("123teste ./23@teste") == "teste teste"
 
 
 def test_remove_letras():
-    texto = "123teste ./23@teste"
+    assert remove_letras("123teste ./23@teste") == "123 ./23@"
 
-    result = remove_letras(texto)
-    assert result == "123 ./23@"
+def test_extrai_data_mes_ano():
+    assert extrai_data_mes_ano("Un. 11/25") == "11/25"
 
+def test_normalizar_mes():
+    assert normalizar_mes("Un. 11/25") == 11
+    assert normalizar_mes("nov") == 11
+    assert normalizar_mes("novembro") == 11
+    assert normalizar_mes("nov/25") == 11
+    assert normalizar_mes("teste nov-24") == 11
+    assert normalizar_mes("teste novembrorogo") == None
 
